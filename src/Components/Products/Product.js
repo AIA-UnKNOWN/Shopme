8b40a2091131d7a347ea39b49cab63cdeaffecaf
AIA-UnKNOWN <ajboyian321@gmail.com>
2021-04-17 21:18:57 -0700

    Fixes decrement button where it lacks padding in Product IU and in CartItem UI

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -11,9 +11,9 @@ function Product(props) {
 				<div className="price-quantity">
 					<span className="price">${props.productPrice}</span>
 					<span className="quantity-wrapper">
-						<button className="add-quantity">+</button>
-						<span className="quantity">1</span>
 						<button className="subt-quantity">-</button>
+						<span className="quantity">1</span>
+						<button className="add-quantity">+</button>
 					</span>
 				</div>
 				<button className="add-to-cart" onClick={props.addToCart} name={props.productTitle}>Add to cart</button>
